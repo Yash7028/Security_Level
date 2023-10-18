d1f6c6f50914bdccbd45aeafe2f2696725c001fd
Yash Batavle <yashbatavle25y@gmail.com>
2023-10-19 02:05:46 +0530

    cookies and session create

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
-// const md5 = require("md5");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
-
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
@@ -15,17 +14,31 @@ app.use(express.static("public"));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
+//session
+app.use(session({
+    secret:"Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+// passport to manage session
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
 const userSchema =new mongoose.Schema({
     email : String ,
     password : String 
 });
-// encryption 
 
-userSchema.plugin(encrypt , {secret : process.env.API_KEY, encryptedFields: ['password']});
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
     res.render("home");
@@ -39,55 +52,50 @@ app.get("/register", function(req, res){
     res.render("register");
 });
 
-app.post("/register", function(req,res){
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        // Store hash in your password DB.
-        const newUser = new User({
-            email : req.body.username ,
-            password : hash
-            // password : md5(req.body.password) 
-           });
-            newUser.save()
-            .then((comregister) => {
-            res.render("secrets");
-            })
-            .catch((err) => {
-                console.log(err);
-            });
+
+app.get("/secrets", function(req, res){
+    if(req.isAuthenticated()){
+        res.render("secrets"); // Remove the leading forward slash and file extension
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.get('/logout', function(req, res, next){
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      res.redirect('/');
     });
+  });
 
-
+app.post("/register", function(req, res) {
+    User.register({username: req.body.username}, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets");
+            });
+        }
+    });
 });
 
 app.post("/login", function (req, res) {
-    
-    const username = req.body.username;
-    const password = req.body.password;
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
 
-    User.findOne({ email: username }).exec()
-        .then((foundUser) => {
-            if (foundUser) {
-                    // res.render("secrets");
-                    // console.log(foundUser.password);
-                    bcrypt.compare(password, foundUser.password, function(err, result) {
-                        // result == true
-                        if(result == true){
-                            res.render("secrets");
-                        }
-                            else{
-                                console.log(err);
-                            }
-        })
-            } else {
-                // User not found
-                res.status(404).send("User not found");
-            }
-        })
-        .catch(err => {
-            console.log(err);
-            // Handle the error, for example, by sending an error response.
-            res.status(500).send("An error occurred.");
-        });
+    req.login(user, function(err){
+        if(err){
+        console.log(err);
+        }else{
+            passport.authenticate("local")(req,res, function(){
+                res.redirect("/secrets");
+            })
+        }
+    });
 });
 
 
